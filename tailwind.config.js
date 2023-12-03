522d77608912451e0565587a7e0a6d336bdb9aae
axelcabanas <axelca72@gmail.com>
2023-12-03 04:24:09 -0300

    configure tailwind container style

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,17 @@ export default {
   ],
   theme: {
     extend: {},
+    container: {
+      center: true,
+      padding: '4vw',
+      screens: {
+        sm: '1600px',
+        md: '1600px',
+        lg: '1600px',
+        xl: '1600px',
+        '2xl': '1600px',
+      },
+    },
   },
   plugins: [],
 }
